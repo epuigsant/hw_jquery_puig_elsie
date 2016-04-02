7a62d7908e76fb738445b8a3a2b81e259650992c
Elsie Puig <epuigsant@gmail.com>
2016-04-01 17:01:23 -0700

    change styling of paragraphs using jquery

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$( document ).ready( function() {
+    var paragraphs = $( 'p' );
+    paragraphs.css( { 'font-family': 'arial', 'lineHeight': 1.5, 'color': 'blue', 'width': '600px', 'padding-left': '15px' } );
+} );
