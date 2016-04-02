d0879226d7010dcc2d2d07cdec1c5e654cefd53a
Elsie Puig <epuigsant@gmail.com>
2016-04-01 17:02:37 -0700

    fix font-family for paragraphs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $( document ).ready( function() {
     var paragraphs = $( 'p' );
-    paragraphs.css( { 'font-family': 'arial', 'lineHeight': 1.5, 'color': 'blue', 'width': '600px', 'padding-left': '15px' } );
+    paragraphs.css( { 'font-family': 'sans-serif', 'lineHeight': 1.5, 'color': 'blue', 'width': '600px', 'padding-left': '15px' } );
 } );
